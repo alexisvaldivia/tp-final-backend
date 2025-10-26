ae7684282ea5f7fdbac247e9dd7f529509226f25
alexis <alesixvaldivia@gmail.com>
2025-10-26 10:42:13 -0300

    Creo DTO para la actualizacion de datos de un usuario

--- src/modules/users/user.dto.js
+++ src/modules/users/user.dto.js
@@ -14,3 +14,11 @@ export const loginSchema = Joi.object({
 	email: Joi.string().email().required(),
 	password: Joi.string().required(),
 });
+
+export const updateSchema = Joi.object({
+	name: Joi.string(),
+	surname: Joi.string(),
+	email: Joi.string().email(),
+	password: Joi.string(),
+	role: Joi.string().valid('emprendedor', 'empresa', 'municipalidad', 'admin'),
+});
